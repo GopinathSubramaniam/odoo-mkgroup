e9230d5ced3c3bbaa6a8c36aa0e8549c496301bf
Gopi <gopiwrld@gmail.com>
2025-01-15 15:52:24 +0530

    Added custom pdf module

---
+++ custom_pdf_reports/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Custom PDF Reports",
+    "version": "18.0.1.0",
+    "author": "Sygmetiv",
+    "website": "https://www.sygmetiv.com",
+    "depends": ['base', 'web', 'account' ],
+    "data": [
+        "views/invoice_report.xml",
+    ],
+    'installable': True,
+    'application': False,
+}
