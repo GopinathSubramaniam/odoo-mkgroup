19a03a9d59898afed23edb645c7dfe237037b139
Rizwaan Salim <argenspin@gmail.com>
2025-01-17 11:49:21 +0530

    Changes to quotation report

--- custom_pdf_reports/__manifest__.py
+++ custom_pdf_reports/__manifest__.py
@@ -3,9 +3,10 @@
     "version": "1.0",
     "author": "Sygmetiv",
     "website": "https://www.sygmetiv.com",
-    "depends": ['base', 'web', 'account',],
+    "depends": ['base', 'web', 'account', 'sale_management'],
     "data": [
         "views/invoice_report.xml",
+        "views/quotation_report.xml",
     ],
     'installable': True,
     'application': False,
