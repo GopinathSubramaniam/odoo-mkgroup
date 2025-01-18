dc76f38e7867991779c49eb3fb37fe426d5b2ffb
Rizwaan Salim <argenspin@gmail.com>
2025-01-18 09:13:09 +0530

    Added some missing dependencies. Avoid breaking the bank details table in invoice report

--- custom_pdf_reports/__manifest__.py
+++ custom_pdf_reports/__manifest__.py
@@ -3,7 +3,7 @@
     "version": "1.0",
     "author": "Sygmetiv",
     "website": "https://www.sygmetiv.com",
-    "depends": ['base', 'web', 'account', 'l10n_sa', 'l10n_sa_edi' ,'sale_management'],
+    "depends": ['base', 'web', 'account', 'l10n_sa', 'l10n_sa_edi' ,'sale_management', 'stock', 'purchase', 'partner_arabic_custom', 'product_arabic_custom'],
     "data": [
         "views/layouts.xml",
         "views/sale_form.xml",
